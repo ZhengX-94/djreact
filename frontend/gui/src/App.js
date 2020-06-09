7e4d0a8f962a2fd88ffb659633c6ea440fd15f45
Zheng <ZhengXu94@users.noreply.github.com>
2020-06-08 21:44:04 -0400

    added frontend and backend

---
+++ frontend/gui/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import BaseRouter from './routes';
+// import logo from './logo.svg';
+import './App.css';
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+
+import CustomLayout from './containers/Layout';
+import ArticleList from './containers/ArticleListView';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <CustomLayout>
+          <BaseRouter />
+        </CustomLayout>
+      </Router>
+
+    </div>
+  );
+}
+
+export default App;
